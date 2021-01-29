f49a114f0e43746d66721b0d68f8ea95f7df6a7f
prasanth k <prasanth.kesavarapu@phenompeople.com>
2021-01-29 16:18:01 +0530

    http dsl with more options

--- test.py
+++ test.py
@@ -4,28 +4,45 @@ import sys
 mm = metamodel_from_file('http.tx')
 
 
-
 def apply(mm, filename):
     import requests
-    with open(filename,'r') as f:
+    with open(filename, 'r') as f:
         httpData = f.read()
-    model =mm.model_from_str(httpData)
+    model = mm.model_from_str(httpData)
     req = requests.Request()
     req.url = model.http.url
     req.method = model.method.type if model.method else 'GET'
-    if model.headerlist:
-        for header in model.headerlist.headers:
-            req.headers[header.key] = header.value
-    if model.querylist:
-        req.params = {}
-        for query in model.querylist.querys:
-            req.params[query.key] = query.value
+    headers = {}
+    params = {}
+    if model.lines:
+        lines = model.lines.lines
+        for line in lines:
+            if line.query:
+                params[line.query.key] = line.query.value
+            else:
+                headers[line.header.key] = line.header.value
+    req.headers.update(headers)
+    req.params.update(params)
+    if (model.payload):
+        if model.payload.data:
+            req.data = model.payload.data
+        else:
+            with open(model.payload.file, 'r') as f:
+                data = f.read()
+            req.data = data
     session = requests.session()
     prepared = req.prepare()
-    return session.send(prepared)
+    resp = session.send(prepared)
+    if model.output:
+        with open(model.output.output, 'w') as f:
+            f.write(resp.text)
+    else:
+        print(resp.text)
 
-print(sys.argv)
-if len(sys.argv)  == 2:
-    print(apply(mm, sys.argv[1]).text)
-else:
-    print('run with python test.py filename.http')
+
+if __name__ == "__main__":
+    print(sys.argv)
+    if len(sys.argv) == 2:
+        print(apply(mm, sys.argv[1]).text)
+    else:
+        print('run with python test.py filename.http')
