c4d6b2e0fd3a39edabbce56e459a7a5b6f57bdef
Prasanth <kesavarapu.siva@gmail.com>
2021-01-30 18:42:17 +0530

    revamp

---
+++ README.md
@@ -0,0 +1,10 @@
+# Inspiration
+
+With the rise in usage of microservices, using http/curl is essential part of most developer's jobs. There are multiple options out there (some are curl, postmant) . My Ideal choice would be to use curl but problems with it is having no history. Postman solves that problem but once user logs in, postman actually syncs it to their servers which i did not like. 
+
+## GOAL
+dothttp will provide simple, cleaner architecture for making http requests. It uses xtext (eclipse developed dsl) to build a custom dsl.
+
+
+
+checkout [examples]('./examples/httpbinorg.http')
--- dothttp/__init__.py
+++ dothttp/__init__.py
@@ -1,7 +1,9 @@
 from textx import metamodel_from_file, get_children_of_type
 import sys
+import re
 
 mm = metamodel_from_file('http.tx')
+variableexp = re.compile(r'\{(?P<word>\w*)\}')
 
 
 def apply(mm, filename):
@@ -11,12 +13,11 @@ def apply(mm, filename):
     model = mm.model_from_str(httpData)
     req = requests.Request()
     req.url = model.http.url
-    req.method = model.method.type if model.method else 'GET'
+    req.method = model.http.method if model.http.method else 'GET'
     headers = {}
     params = {}
     if model.lines:
-        lines = model.lines.lines
-        for line in lines:
+        for line in model.lines:
             if line.query:
                 params[line.query.key] = line.query.value
             else:
@@ -45,5 +46,4 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         print(apply(mm, sys.argv[1]))
     else:
-        print(apply(mm, 'workday-upload-delete.http'))
         print('run with python test.py filename.http')
