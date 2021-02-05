31b8903d51d0eedcbbb0b02a233d253b044735d9
Prasanth <kesavarapu.siva@gmail.com>
2021-02-05 10:53:48 +0530

    json test cases. need to add substituion tests

--- tests/requesttest.py
+++ tests/requesttest.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from dothttp import Config, RequestCompiler
@@ -58,6 +59,97 @@ class RequestTest(unittest.TestCase):
             '{"string": "simple", "list": ["dothttp", "azure"], "null": null, "bool": false, "bool2": true, "float": 1.121212, "float2": 1.0}',
             req.body,
             "incorrect method")
+
+    def test_json_payload3(self):
+        req = self.get_request("tests/requests/jsonpayload3.http")
+        self.assertEqual("https://dothttp.azurewebsites.net/", req.url,
+                         "incorrect url computed")
+        self.assertEqual("POST", req.method, "incorrect method")
+        self.assertEqual({"menu": {
+            "header": "SVG Viewer",
+            "items": [
+                {"id": "Open"},
+                {"id": "OpenNew", "label": "Open New"},
+                None,
+                {"id": "ZoomIn", "label": "Zoom In"},
+                {"id": "ZoomOut", "label": "Zoom Out"},
+                {"id": "OriginalView", "label": "Original View"},
+                None,
+                {"id": "Quality"},
+                {"id": "Pause"},
+                {"id": "Mute"},
+                None,
+                {"id": "Find", "label": "Find..."},
+                {"id": "FindAgain", "label": "Find Again"},
+                {"id": "Copy"},
+                {"id": "CopyAgain", "label": "Copy Again"},
+                {"id": "CopySVG", "label": "Copy SVG"},
+                {"id": "ViewSVG", "label": "View SVG"},
+                {"id": "ViewSource", "label": "View Source"},
+                {"id": "SaveAs", "label": "Save As"},
+                None,
+                {"id": "Help"},
+                {"id": "About", "label": "About Adobe CVG Viewer..."}
+            ]
+        }}, json.loads(req.body), "json payload parsed wrong")
+
+    def test_json_payload_complex(self):
+        req = self.get_request("tests/requests/jsonpayload4.http")
+        self.assertEqual("https://dothttp.azurewebsites.net/", req.url,
+                         "incorrect url computed")
+        self.assertEqual("POST", req.method, "incorrect method")
+        self.assertEqual({
+            "glossary": {
+                "title": "example glossary",
+                "GlossDiv": {
+                    "title": "S",
+                    "GlossList": {
+                        "GlossEntry": {
+                            "ID": "SGML",
+                            "SortAs": "SGML",
+                            "GlossTerm": "Standard Generalized Markup Language",
+                            "Acronym": "SGML",
+                            "Abbrev": "ISO 8879:1986",
+                            "GlossDef": {
+                                "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                                "GlossSeeAlso": ["GML", "XML"]
+                            },
+                            "GlossSee": "markup"
+                        }
+                    }
+                }
+            }
+        }, json.loads(req.body), "json payload parsed wrong")
+
+    def test_substition(self):
+        # TODO
+        # host, query, data
+        pass
+
+    def test_substition2(self):
+        # TODO
+        # json
+        pass
+
+    def test_substition3(self):
+        # TODO
+        # test multiple variables
+        pass
+
+    def test_substition4(self):
+        # TODO
+        # test multiple env
+        pass
+
+    def test_substition5(self):
+        # TODO
+        # test default, specifed property file
+        pass
+
+    def test_substition5(self):
+        # TODO
+        # comments in payload
+        pass
 
     @staticmethod
     def get_request(file):
