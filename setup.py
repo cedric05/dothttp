17bcb49b792f9634c0a56f5b46320b956b47b4b1
Prasanth <kesavarapu.siva@gmail.com>
2021-03-06 17:49:34 +0530

    version update

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def requirements():
 
 setup(
     name="dothttp_req",
-    version="0.0.7b",
+    version="0.0.7",
     author="prasanth",
     author_email="kesavarapu.siva@gmail.com",
     description=("DotHttp recommended tool for making http requests."),
