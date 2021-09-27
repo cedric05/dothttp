b1d19d30b10fbd16b99521256dd56f4c113cb741
Prasanth <kesavarapu.siva@gmail.com>
2021-09-27 10:32:00 +0530

    [Feature] provide a way to include contexts (cells request data) while executing content.
    ----
    using this feature, dothttp-runner will send contexts while content-execute (cell execution). which will allow it to reuse (leverage dothttp extend to define auth, headers, base url)

--- test/extensions/test_content.py
+++ test/extensions/test_content.py
@@ -1,4 +1,5 @@
 import json
+from typing import Union
 
 from dotextensions.server.handlers.basic_handlers import ContentExecuteHandler
 from dotextensions.server.models import Command
@@ -25,14 +26,17 @@ class FileExecute(TestBase):
         result = self.execute_and_result("GET https://httpbin.org/post")
         self.assertEqual(405, result.result['status'])
 
-    def execute_and_result(self, content):
+    def execute_and_result(self, content, target: Union[str, int] = 1, contexts=None, curl=False):
         return self.execute_handler.run(Command(
             method=ContentExecuteHandler.name,
             params={
                 "content": content,
                 "properties": {
                     "post": "get"
-                }
+                },
+                "target": target,
+                "contexts": contexts,
+                "curl": curl
             },
             id=1)
         )
@@ -45,3 +49,71 @@ class FileExecute(TestBase):
         """env doesnt live as it has no relative .dothttp.json path"""
         result = self.execute_and_result("GET https://httpbin.org/{{post}}")
         self.compare_results(result, "https://httpbin.org/get")
+
+    def test_execute_content_context(self):
+        # tests to confirm contexts can be used
+        # (curl and run)
+        kwargs = dict(
+            content=
+            """
+            @name('test'):"base"
+            POST ""
+            json({
+                "hai":"{{testvar}}"
+            })
+            """, target="test",
+            contexts=[
+                # invalid context
+                "",
+                # valid content
+                """
+                @name("base")
+                // {{testvar=bye}}
+                POST https://httpbin.org/post
+                """])
+        self.assertEqual('''curl -X POST --url https://httpbin.org/post \\
+-H \'content-type: application/json\' \\
+-d \'{
+    "hai": "bye"
+}\'''', self.execute_and_result(curl=True, **kwargs).result['body'])
+
+        self.assertEqual(200, self.execute_and_result(curl=False, **kwargs).result['status'])
+
+    def test_execute_content_context_error(self):
+        # tests to throw when base is not found
+        result = self.execute_and_result(
+            """
+            @name('test'):"base"
+            POST "https://google.com"
+            """, target="test", contexts=["", ""], curl=True)
+        self.assertTrue(result.result['error'])
+        self.assertEqual('http def with name `base` not defined for http  with name `test`',
+                         result.result['error_message'])
+
+    def test_execute_content_context_multiple_base(self):
+        # tests to pick first context if multiple contexts are there
+        result = self.execute_and_result(
+            """
+            @name('test'):"base"
+            POST ""
+            """, target="test", contexts=[
+                # as it is not single file
+                # there can be multiple targets
+                # defined with same name
+                # in such scenario
+                # only first one will be picked
+                """
+                @name('base')
+                POST "https://req.dothttp.dev/ram"
+                """,
+                """
+                @name('base')
+                POST "https://req.dothttp.dev/krishna"
+                """], curl=True)
+        # picks first one out multiple similar bases
+        # same behaviour will not be observed in case of http file
+        # in http file, it will throw error mentioning duplicate target found
+        self.assertEqual(
+            '''curl -X POST --url https://req.dothttp.dev/ram \\
+''',
+            result.result['body'])
