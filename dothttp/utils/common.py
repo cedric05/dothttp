93a1a921a47c439abcab0e6435496956c886c929
Prasanth <kesavarapu.siva@gmail.com>
2024-02-25 15:06:26 +0530

    normalize path for dothttp to work for pyinstaller

--- dothttp/utils/common.py
+++ dothttp/utils/common.py
@@ -11,13 +11,16 @@ def get_real_file_path(
     current_file=__file__,
 ):
     if os.path.exists(current_file):
-        tx_model_path = os.path.join(
-            os.path.dirname(os.path.abspath(current_file)), path
+        tx_model_path = os.path.normpath(
+            os.path.join(
+                os.path.dirname(os.path.abspath(current_file)), 
+                path
+            )
         )
     else:
-        tx_model_path = os.path.join(
+        tx_model_path = os.normpath(os.path.normpath(os.path.join(
             os.path.dirname(os.path.dirname(os.path.abspath(current_file))), path
-        )
+        )))
     return tx_model_path
 
 
